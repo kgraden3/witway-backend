b7320ba0deea97b73f16aa34b8a2d205888df3e3
Ryan Brandt <rbrandt810@gmail.com>
2019-05-24 18:30:11 -0400

    master: mid fix

--- witway_backend/backend/admin.py
+++ witway_backend/backend/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(UserDetails)
+admin.site.register(UserDetail)
 admin.site.register(Stake)
 admin.site.register(Account)
--- witway_backend/backend/models.py
+++ witway_backend/backend/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class UserDetails(models.Model):
+class UserDetail(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     gender = models.CharField(max_length=50)
     address = models.CharField(max_length=100)
