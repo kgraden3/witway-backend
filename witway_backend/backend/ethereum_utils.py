9987b590d58d88594ffdf8d316babb0c68e167c7
Ryan Brandt <rbrandt810@gmail.com>
2019-05-24 23:35:41 -0400

    ethereum: interating on tests and connection to infura

--- witway_backend/backend/ethereum_utils.py
+++ witway_backend/backend/ethereum_utils.py
@@ -1,6 +1,29 @@
-import web3
+from web3 import Web3
 import os
 from django.conf import settings
 
-def getKeystore():
-    return open(os.path.join(settings.BASE_DIR, 'witway-keystore'))
+
+class EthereumUtils:
+
+    def __init__(self):
+        provider = Web3.WebsocketProvider('wss://ropsten.infura.io/ws/v3/feb06b6a31854be7b91a77f307fc94cc')
+        self.w3 = Web3(provider)
+        self.address ='0x5b7bd33cfa8f37951e78d5a52436c2e65a8b0d83'
+        private_key = self.decrypt_keystore()
+
+
+    def get_keystore(self):
+        return open(os.path.join(settings.BASE_DIR, 'witway-keystore')).read()
+
+    def decrypt_keystore(self):
+        keystore = self.get_keystore()
+        private_key = self.w3.eth.account.decrypt(keystore, 'hackathon')
+        return private_key
+
+    # def get_gas_estimate(self, transaction):
+    #     self.w3.eth.estimateGas()
+    #
+    # def get_account(self):
+    #     return self.account
+
+    # def create_transaction(self, to, value):
--- witway_backend/backend/tests.py
+++ witway_backend/backend/tests.py
@@ -1,5 +1,5 @@
 from django.test import SimpleTestCase
-from . import ethereum_utils
+from .ethereum_utils import EthereumUtils
 
 
 # Create your tests here.
@@ -7,8 +7,13 @@ from . import ethereum_utils
 class EthereumUtilsTestCase(SimpleTestCase):
 
     def setUp(self):
+        self.utils = EthereumUtils()
         pass
 
     def test_get_keystore(self):
-        keystore = ethereum_utils.getKeystore()
+        keystore = self.utils.get_keystore()
         self.assertIsNotNone(keystore)
+
+    def test_decrypt_keystore(self):
+        private_key = self.utils.decrypt_keystore()
+        print(private_key)
